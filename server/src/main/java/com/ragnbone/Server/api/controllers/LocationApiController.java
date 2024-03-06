357a4b68f5a0606848a374cf847abe846b9daf0d
KerkZz <49468231+KerkZz@users.noreply.github.com>
2024-03-06 19:08:27 +0800

    Read and Write Location

--- server/src/main/java/com/ragnbone/Server/api/controllers/LocationApiController.java
+++ server/src/main/java/com/ragnbone/Server/api/controllers/LocationApiController.java
@@ -1,5 +1,38 @@
 package com.ragnbone.Server.api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ragnbone.Server.api.repository.LocationRepository;
+import com.ragnbone.Server.api.service.LocationService;
+import com.ragnbone.Server.models.Location;
+
+@RestController
+@RequestMapping("/api/v1/location")
 public class LocationApiController {
-    
+
+    @Autowired
+    private LocationService locationService;
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @PostMapping("/update")
+    public ResponseEntity<Void> updateLocation(@RequestBody Location location) {
+        locationService.saveLocationUpdate(location);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity<List<Location>> readLocation() {
+        List<Location> matching = locationRepository.readLocation();
+        return ResponseEntity.ok(matching);
+    } 
 }
