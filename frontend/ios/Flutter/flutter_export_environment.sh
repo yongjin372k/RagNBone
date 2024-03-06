33168d3b24a6285fdb73b2be2b31632d9bf69311
Ewan <ewanhow@gmail.com>
2024-03-06 20:37:56 +0800

    Front Page + Mapping(Trying)

--- frontend/ios/Flutter/flutter_export_environment.sh
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\ewanh\OneDrive\Documents\flutter_windows_3.13.7-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ewanh\java2\rag_and_bone"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ewanh\java2\RagNBone\frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- frontend/lib/Screens/RegisterPage.dart
+++ frontend/lib/Screens/RegisterPage.dart
@@ -1,108 +1,329 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatefulWidget {
-  @override
-  _RegisterPageState createState() => _RegisterPageState();
+void main() {
+  runApp(MyApp());
 }
 
-class _RegisterPageState extends State<RegisterPage> {
-  bool _isMerchant = false; // Initialize as customer
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: RegisterPage(),
+      ),
+    );
+  }
+}
 
+class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Register'),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start, // Change to MainAxisAlignment.start
-          children: [
-            Text(
-              'Create an Account',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              // ... Existing username field
-              style: TextStyle(fontSize: 18),
-              decoration: InputDecoration(
-                labelText: 'Username',
-                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              // ... Existing email field
-              style: TextStyle(fontSize: 18),
-              decoration: InputDecoration(
-                labelText: 'Email',
-                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              // ... Existing password field
-              style: TextStyle(fontSize: 18),
-              decoration: InputDecoration(
-                labelText: 'Password',
-                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              ),
-            ),
-            SizedBox(height: 20), // Add a gap between the password field and the next widget
-            ElevatedButton(
-              onPressed: () {
-                // Implement registration functionality
-              },
-              child: Text('Register'),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green,
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                textStyle: TextStyle(fontSize: 18),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+      body: Container(
+        width: 430,
+        height: 932,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 245, 238, 1),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 260,
+              left: 61,
+              child: Container(
+                width: 308,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    )
+                  ],
+                  color: Color.fromRGBO(0, 0, 0, 1),
                 ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Already Have an Account?'),
-                TextButton(
-                  onPressed: () {
-                    // Implement login navigation
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.green),
+            Positioned(
+              top: 330,
+              left: 61,
+              child: Container(
+                width: 308,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    )
+                  ],
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 400,
+              left: 61,
+              child: Container(
+                width: 308,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    )
+                  ],
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 470,
+              left: 61,
+              child: Container(
+                width: 308,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    )
+                  ],
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Container(
+                width: 430,
+                height: 47,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(217, 217, 217, 1),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 84,
+              left: 80,
+              child: Container(
+                width: 269,
+                height: 117,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Karanggunilogo.png'),
+                    fit: BoxFit.fitWidth,
                   ),
                 ),
-              ],
+              ),
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Role:'),
-                SizedBox(width: 10),
-                Switch(
-                  value: _isMerchant,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _isMerchant = newValue;
-                    });
-                  },
-                  activeColor: Colors.green,
-                  activeTrackColor: Colors.lightGreenAccent,
+            Positioned(
+              top: 223,
+              left: 24,
+              right: 24,
+              child: Text(
+                'Let’s make magic happen!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                  fontFamily: 'Istok Web',
+                  fontSize: 18,
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                  height: 1,
                 ),
-                Text(_isMerchant ? 'Merchant' : 'Customer'),
-              ],
+              ),
+            ),
+            Positioned(
+              top: 280,
+              left: 24,
+              right: 24,
+              child: Text(
+                'What’s your name?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 245, 238, 1),
+                  fontFamily: 'Istok Web',
+                  fontSize: 14,
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                  height: 1,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 355,
+              left: 24,
+              right: 24,
+              child: Text(
+                'What’s your phone number?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 245, 238, 1),
+                  fontFamily: 'Istok Web',
+                  fontSize: 14,
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                  height: 1,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 425,
+              left: 24,
+              right: 24,
+              child: Text(
+                'Create a password!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 245, 238, 1),
+                  fontFamily: 'Istok Web',
+                  fontSize: 14,
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                  height: 1,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 495,
+              left: 24,
+              right: 24,
+              child: Text(
+                'Reconfirm your password!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 245, 238, 1),
+                  fontFamily: 'Istok Web',
+                  fontSize: 14,
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                  height: 1,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 550,
+              left: 24,
+              right: 24,
+              child: Text(
+                'I would like to sign up as a:',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                  fontFamily: 'Istok Web',
+                  fontSize: 16,
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                  height: 1,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 650,
+              left: 24,
+              right: 24,
+              child: Text(
+                'By signing up, you agree to the terms & conditions.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                  fontFamily: 'Istok Web',
+                  fontSize: 12,
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                  height: 1,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 590,
+              left: 90,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    )
+                  ],
+                  color: Color.fromRGBO(167, 17, 113, 1),
+                  border: Border.all(
+                    color: Color.fromRGBO(19, 98, 9, 1),
+                    width: 1,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      'Karang Guni',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 245, 238, 1),
+                        fontFamily: 'Istok Web',
+                        fontSize: 14,
+                        letterSpacing: 0,
+                        fontWeight: FontWeight.normal,
+                        height: 1.2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 590,
+              left: 222,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    )
+                  ],
+                  color: Color.fromRGBO(167, 17, 113, 1),
+                  border: Border.all(
+                    color: Color.fromRGBO(19, 98, 9, 1),
+                    width: 1,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      'Donor/Seller',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 245, 238, 1),
+                        fontFamily: 'Istok Web',
+                        fontSize: 14,
+                        letterSpacing: 0,
+                        fontWeight: FontWeight.normal,
+                        height: 1.2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
